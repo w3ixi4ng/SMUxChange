efd35c14ffa9f73265806d6976152cc2201ff035
weixiang <weixiang0124@gmail.com>
2025-10-07 21:30:23 +0800

    Splitted Frontend and Backend Folders

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+    <Button>This is a Shadcn UI Button</Button>
+    </>
+  )
+}
+
+export default Home
