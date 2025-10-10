2360c76783dec9b6606856e6a38ed0e46e448f8e
weixiang <weixiang0124@gmail.com>
2025-10-11 00:38:17 +0800

    icons

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -3,28 +3,28 @@ import Home from '../pages/Home.tsx';
 import Information from '../pages/Information.tsx';
 import Home2 from '../pages/Home2.tsx';
 import MappableV3 from '../pages/MappableV3.tsx';
-import { Plane } from 'lucide-react';
+import { Plane, House, MapIcon, GraduationCap } from 'lucide-react';
 
 function RouterView() {
     return (
         <Router>
             <div className="min-h-screen bg-neutral-500">
-                <nav className="bg-neutral-800 shadow-sm py-3">
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <nav className="bg-neutral-800 shadow-sm">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" style={{ fontFamily: 'Roboto' }}>
                         <div className="flex h-16">
                             <div className="flex items-center justify-between w-full space-x-8">
                                 <Link to="/" className="text-2xl font-bold text-white text-decoration-none">
                                     <Plane className="inline-block" /> SMUxChange
                                 </Link>
                                 <div className="flex space-x-4">
-                                    <Link to="/" className="text-2xl font-bold text-white text-decoration-none">
-                                        Home
+                                    <Link to="/" className="text-xl font-bold text-white text-decoration-none">
+                                    <House className="inline-block" /> Home
                                     </Link>
-                                    <Link to="/mappablev3" className="text-2xl font-bold text-white text-decoration-none">
-                                        MappableV3
+                                    <Link to="/mappablev3" className="text-xl font-bold text-white text-decoration-none">
+                                        <MapIcon className="inline-block" /> Map
                                     </Link>
-                                    <Link to="/information" className="text-2xl font-bold text-white text-decoration-none">
-                                        Information
+                                    <Link to="/information" className="text-xl font-bold text-white text-decoration-none">
+                                        <GraduationCap className="inline-block" /> Schools
                                     </Link>
                                 </div>
                             </div>
