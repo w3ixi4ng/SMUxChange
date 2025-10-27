9c993975c8c21b1e09fe2f65608da8e5171ce5fb
w3ixi4ng <weixiang0124@gmail.com>
2025-10-28 02:09:24 +0800

    update map logic p1, update mappable logic to match

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -89,7 +89,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="../public/images/oxford.jpg"
+              src="/images/oxford.jpg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -103,7 +103,7 @@ export function SignupForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="../public/images/oxford.jpg"
+              src="/images/oxford.jpg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
