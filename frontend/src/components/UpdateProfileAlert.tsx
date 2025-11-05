914807a2366b0d775973fa1d1764d3b97ca1f1f5
w3ixi4ng <weixiang0124@gmail.com>
2025-11-05 16:20:22 +0800

    update modal

--- frontend/src/components/UpdateProfileAlert.tsx
+++ frontend/src/components/UpdateProfileAlert.tsx
@@ -38,23 +38,33 @@ export function UpdateProfileAlert({ uid, name, faculty, major, track, secondMaj
         }
         if (errors.length > 0) {
             setErrorMessage(errors);
+            toast.error("Profile update failed", {
+                description: "Please check your inputs and try again.",
+            });
             return;
-        }
+        } else {
 
-        try {
-            await axios.post('https://smuxchange-backend.vercel.app/database/saveProfile', { uid, name, faculty, major, track, secondMajor });
-            setErrorMessage([]);
-        } catch (error) {
-            console.log("API error saving profile:", error);
+            try {
+                await axios.post('https://smuxchange-backend.vercel.app/database/saveProfile', { uid, name, faculty, major, track, secondMajor });
+                setErrorMessage([]);
+                toast.success("Profile updated successfully", {
+                    description: "Your profile has been updated successfully.",
+                });
+            } catch (error) {
+                console.log("API error saving profile:", error);
+                toast.error("Profile update failed", {
+                    description: "Please check your inputs and try again.",
+                });
+            }
         }
     };
 
     return (
-        
+
         <AlertDialog>
             <AlertDialogTrigger asChild>
                 <button className="bg-white text-black font-semibold hover:bg-gray-200 hover:scale-105 transition-transform px-8 py-2 text-lg rounded-full shadow-lg rounded">
-                    Edit Profile
+                    Update Profile
                 </button>
             </AlertDialogTrigger>
             <AlertDialogContent>
@@ -68,9 +78,6 @@ export function UpdateProfileAlert({ uid, name, faculty, major, track, secondMaj
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
                     <AlertDialogAction onClick={() => {
                         saveProfile(uid || "", name, faculty, major, track, secondMajor);
-                        toast.success("Profile updated successfully", {
-                            description: "Your profile has been updated successfully.",
-                        });
                     }}>
                         Update
                     </AlertDialogAction>
